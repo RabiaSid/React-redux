0dc0a70d1f24f1ed09a912d853927efffb2904dc
RabiaSid <rabiasid984@gmail.com>
2023-05-22 21:49:38 +0500

    post get by inputfield2

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import {selectAllPosts} from "./postsSlice"
 
 const PostsList = () => {
-const posts = useSelector( state=> state.posts)
+const posts = useSelector(selectAllPosts)
 
 const renderedPosts = posts.map(post =>(
     <article key ={post.id}>
